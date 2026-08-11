68fe50e2b570518f69fe107ca417b5fdbe3bc1ba
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-08-11 12:20:10 +0100

    modify main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from github-actions!")
+from flask import Flask
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+
+@app.route("/<random_string>")
+def return_backwards_string(random_string):
+    return "".join(reversed(random_string))
+
+
+@app.route("/get-mode")
+def get_mode():
+    return os.getenv("MODE")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=8080)
